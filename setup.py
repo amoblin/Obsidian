f4c1d86d4def534a61560b959a7a29caa44a4740
amoblin@mbp <amoblin@gmail.com>
2016-09-19 16:03:59 +0800

    Obsidian 1.0

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# file name: setup.py
-# description: TODO
-# create date: 2016-09-19 13:37:41
-# author: amoblin
-# This file is created by Marboo<http://marboo.io> template file $MARBOO_HOME/.media/starts/default.py
-# 本文件由 Marboo<http://marboo.io> 模板文件 $MARBOO_HOME/.media/starts/default.py 创建
-
 from setuptools import setup, find_packages
 
 setup(
     name = "Obsidian",
-    version = "0.1",
+    version = "1.0",
     packages = find_packages(),
-    scripts = ['obsidian.py'],
+    scripts = ['obsidian'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['docutils>=0.3'],
+    install_requires = [
+        'docutils>=0.3',
+        'scrapy'
+    ],
 
     package_data = {
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
     },
 
     # metadata for upload to PyPI
@@ -31,7 +24,7 @@ setup(
     description = "Obsidian make web crawl easier",
     license = "MIT",
     keywords = "web crawl json",
-    url = "https://github.com/amoblin/Obsidian",   # project home page, if any
+    url = "https://github.com/amoblin/Obsidian",
 
     # could also include long_description, download_url, classifiers, etc.
 )
